bad0f24e644444257b8a3dc6b5d41fbbe53c87f7
Jerry Feng <jerry.feng41@gmail.com>
2016-05-15 03:55:07 -0400

    refactor boys

--- js/game.js
+++ js/game.js
@@ -1,61 +1,32 @@
-var game = new Phaser.Game(1024, 640, Phaser.AUTO, '', 
-    { 
-        preload: preload, 
-        create: create, 
-        update: update,
-        render: render
-    });
+GrilledGame.Game = function(game) {};
 
-var assets = {
-    player: {w: 108, h: 192},
-    lion_run: {w: 102, h: 122},
-    lion_jump: {w: 98, h: 130},
-    fuel: {w: 28, h: 32},
-    goat: {w: 54, h: 60},
-    pole: {w: 8, h: 64},
-    fire: {w: 108, h: 256},
-    meat: {w: 36, h: 24},
-    keys: {w: 144, h: 102}
-}
+var game;
+
+GrilledGame.Game.prototype = {
+    preload: function() {
+        game = this;
+    },
+    create: create,
+    update: update
+};
 
 var fontName = 'Share Tech Mono';
 
+var fontReady = false;
+var textCreated = false;
+
 // http://phaser.io/examples/v2/text/google-webfonts
 WebFontConfig = {
-    active: function() { 
-        // Need delay for some reason
-        game.time.events.add(1000, createText, this);
+    active: function() {
+        fontReady = true;
+        if (game != null && game.state.created) {
+            createText();    
+        }
     },
     google: {
         families: [fontName]
     }
 };
-
-function preload() {
-    game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
-
-    game.load.image('empty', 'assets/empty.png');
-    game.load.image('bg', 'assets/bg.gif');
-    game.load.image('ground_front', 'assets/ground_front.gif');
-    game.load.image('ground_back', 'assets/ground_back.gif');
-    game.load.spritesheet('player1', 'assets/spritesheets/z1.gif', assets.player.w, assets.player.h);
-    game.load.spritesheet('player2', 'assets/spritesheets/z2.gif', assets.player.w, assets.player.h);
-    game.load.spritesheet('lion_run', 'assets/spritesheets/l1.gif', assets.lion_run.w, assets.lion_run.h);
-    game.load.spritesheet('lion_jump', 'assets/spritesheets/l2.gif', assets.lion_jump.w, assets.lion_jump.h);
-    game.load.spritesheet('fuel', 'assets/spritesheets/fuel.gif', assets.fuel.w, assets.fuel.h);
-    game.load.spritesheet('goat', 'assets/spritesheets/goat.gif', assets.goat.w, assets.goat.h);
-    game.load.image('pole', 'assets/pole.gif');
-    game.load.spritesheet('fire1', 'assets/spritesheets/fire1.gif', assets.fire.w, assets.fire.h);
-    game.load.spritesheet('fire2', 'assets/spritesheets/fire2.gif', assets.fire.w, assets.fire.h);
-    game.load.image('player1_label', 'assets/indicator_ref_1.gif');
-    game.load.image('player2_label', 'assets/indicator_ref_2.gif');
-    game.load.image('fuel_bar', 'assets/bar.gif');
-    game.load.image('player1_gameover', 'assets/z1_icon_grilled.gif');
-    game.load.image('player2_gameover', 'assets/z2_icon_grilled.gif');
-    game.load.spritesheet('meat', 'assets/spritesheets/meat.gif', assets.meat.w, assets.meat.h);
-    game.load.spritesheet('keys1', 'assets/spritesheets/keys1.gif', assets.keys.w, assets.keys.h);
-    game.load.spritesheet('keys2', 'assets/spritesheets/keys2.gif', assets.keys.w, assets.keys.h);
-}
 
 var groundHeight = 64;
 var maxFuelAmount = 500;
@@ -85,8 +56,6 @@ var fuelBar1, fuelBar2;
 var scoreCounter1, scoreCounter2;
 var grilledText1, grilledText2;
 var grilledOverlay1, grilledOverlay2;
-
-var textCreated = false;
 
 var title;
 var keys1, keys2;
@@ -163,6 +132,8 @@ function Player(fireSprite, sprite) {
 }
 
 function create() {
+    console.log("Game created");
+
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     // Draw environment
@@ -243,13 +214,18 @@ function create() {
     grilledOverlay2 = game.add.sprite(game.world.width - 54 - 12, 12, 'player2_gameover');
     grilledOverlay2.visible = false;
 
-    game.time.events.add(1500, function() {
-        // If font could not be retrieved, generate text using default font
-        if (!textCreated) {
-            fontName = '';
-            createText();
-        }
-    }, this);
+    if (fontReady) {
+        createText();
+    } else {
+        // Add delay to wait for font to load
+        game.time.events.add(500, function() {
+            // If font could not be retrieved, just use the default font
+            if (!textCreated) {
+                fontName = '';
+                createText();
+            }
+        }, this);
+    }
 }
 
 function createText() {
